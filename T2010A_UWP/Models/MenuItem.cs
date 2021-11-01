bef409c898ac8c730cf750c70a7c43ed77f73df0
hoatq-fptaptech <hoatq4@fpt.edu.vn>
2021-11-01 15:14:47 +0700

    demo layout

---
+++ T2010A_UWP/Models/MenuItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2010A_UWP.Models
+{
+    class MenuItem
+    {
+        public string Name { get; set; } // abtract property -- khi nao nap data vao thi mới tốn ô nhớ -> thích hợp dùng cho các model - entity - DTO (data transfer object)
+        public string MenuPage { get; set; }
+        public string Icon { get; set; }
+        //indexer - ve tim hieu lai
+
+    }
+}
