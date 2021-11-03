c8e9b4b9355255a5270d94f0e92bcd038c58a84f
hoatq-fptaptech <hoatq4@fpt.edu.vn>
2021-11-03 14:21:46 +0700

    demo image

---
+++ T2010A_UWP/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
+namespace T2010A_UWP.Models
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public BitmapImage Img { get; set; }
+    }
+}
--- T2010A_UWP/Pages/Home.xaml.cs
+++ T2010A_UWP/Pages/Home.xaml.cs
@@ -12,7 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
+using T2010A_UWP.Models;
+using Windows.UI.Xaml.Media.Imaging;
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace T2010A_UWP.Pages
@@ -25,6 +26,13 @@ namespace T2010A_UWP.Pages
         public Home()
         {
             this.InitializeComponent();
+            var p1 = new Product() {Name="Quần áo thời trang thu đông",Price=200000,Img = new BitmapImage(new Uri("https://dscnnwjxnwl3f.cloudfront.net/media/catalog/product/cache/584039753b87a8d227764e04fc461e3e/h/a/hawaii_burger_1_2.jpg")) };
+            var p2 = new Product() {Name="Đồ ăn nhanh fastfood",Price=120000, Img = new BitmapImage(new Uri("https://dscnnwjxnwl3f.cloudfront.net/media/catalog/product/cache/584039753b87a8d227764e04fc461e3e/h/a/hawaii_burger_1_2.jpg")) };
+            // local app image
+            var p3 = new Product() {Name="Đồng hồ quả lắc",Price=250000, Img = new BitmapImage(new Uri("ms-appx:///Assets/images.png")) };
+            Products.Items.Add(p1);
+            Products.Items.Add(p2);
+            Products.Items.Add(p3);
         }
     }
 }
